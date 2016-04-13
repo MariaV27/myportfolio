1fde393f670d74432a2e6d917a2eb9bde05be06f
Maria Vintimilla <mariavintimilla@Marias-MacBook-Air.local>
2016-04-13 14:43:42 -0400

    fuxed js

--- js/home.js
+++ js/home.js
@@ -3,14 +3,6 @@ $('.carousel').carousel();
 $(document).ready(function(){
 
 
-$("#repobutton").on("click", function() {
-  
-  $.ajax({
-  type: "GET",
-  url: "https://api.github.com/users/MariaVintimilla/repos",
-  success: tester
-  });
-
 
 function tester(dataFromRepos){
   for (var i = 0; i < dataFromRepos.length; i++) {
